b46539f1176a734598998a142b228b89446550f4
lum1nar <lum1naxu@gmail.com>
2025-12-03 15:30:34 +0800

    function call

--- lua/floating-toc/init.lua
+++ lua/floating-toc/init.lua
@@ -1,1 +1,14 @@
-vim.notify("123456", vim.log.levels.INFO);
+-- vim.notify("123456", vim.log.levels.INFO);
+--
+--
+local M = {};
+
+M.print = function()
+    vim.notify("Inside print function")
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    for _, line in ipairs(lines) do
+        print(line);
+    end
+end
+
+return M
